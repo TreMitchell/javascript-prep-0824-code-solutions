4233770d2f1e21417f22a4be7ae2e17c12559c85
Tré Mitchell <trem963@gmail.com>
2024-08-07 04:44:01 +0000

    Completed prep-javascript-operators-and-expressions exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,52 @@
+//Exercise Part #4
+const number1 = 5;
+const number2 = 12;
+
+const product = number1 * number2;
+console.log('value of product:', product);
+console.log('typeof product:', typeof product);
+
+//Exercise Part #5
+const charge = 15.99;
+const payment = 9.99;
+
+const amountRemaining = charge - payment;
+console.log('amount remaining:', amountRemaining);
+console.log('typeof amount remaining:', typeof amountRemaining);
+
+//Exercise Part #6
+const tests = 85;
+const assignments = 100;
+const final = 95;
+
+const grade = (tests + assignments + final) / 3;
+console.log('Grade results:', grade);
+console.log('data type of grade:', typeof grade);
+
+//Exercise Part #7
+const firstName = 'Tré';
+const lastName = 'Mitchell';
+
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ' + firstName + ' ' + lastName);
+console.log('typeof of fullName: ' + typeof fullName);
+
+//Exercise Part #8
+const pH = 50;
+const isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+//Exercise Part #9
+const numberOfSoldiers = 300;
+
+const isSparta = numberOfSoldiers === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typEof isSparta:', typeof isSparta);
+
+//Exercise Part #10
+let nickname = fullName;
+nickname += ' is the GOAT!';
+console.log('value of nickname:', nickname);
+console.log('typeof nickname:', typeof nickname);
